f09833e2193d3636b3c6a75ad1ca908c2864b4c0
Nick Teets <nickteets@gmail.com>
2024-09-24 11:33:28 -0600

    complete iteration 1

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
 const { readFileSync } = require('fs')
 const { program } = require('commander')
 
-console.log({readFileSync, program})
+program.command("wwcw").description("Return number of lines, words, bytes in a file")
+
+program.option("-c, --bytes <FILENAME>", "Returns the number of bytes in a file")
+
+program.parse(process.argv)
+const options = program.opts()
+
+if (options.bytes) {
+    console.log(noOfBytes(options.bytes) + " " + options.bytes)
+}
+
+function noOfBytes(file) {
+    return readFileSync(file).length
+}
+
